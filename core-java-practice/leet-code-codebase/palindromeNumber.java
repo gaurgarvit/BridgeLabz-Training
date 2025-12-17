f3ed813a0e17d8445498e2e0510e5c0e566dec8f
Garvit <gaurrit8@gmail.com>
2025-12-17 15:07:44 +0530

    finding palindrome of a number

---
+++ core-java-practice/leet-code-codebase/palindromeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class palindromeNumber {
+    public static void main(String[] args) {
+
+        Scanner s= new Scanner(System.in);
+
+        // Input the number
+        System.out.print("Enter a number: ");
+        int num= s.nextInt();
+
+        int org= num;   // original number
+        int reverse= 0;     //reversed number
+
+        // Reverse the number
+        while (num > 0) {
+            int digit= num%10;          // Get last digit
+            reverse= reverse*10 + digit;   // Add digit to reverse number
+            num= num / 10;                // Remove last digit from num
+        }
+
+        // Check if original and reversed are same
+        if (org== reverse) {
+            System.out.println(org + " is a Palindrome Number");
+        } else {
+            System.out.println(org + " is not a Palindrome Number");
+        }
+    }
+}
