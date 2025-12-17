9f0bf8fb94de561c0c58cea7a9816a8baf8cdde7
Garvit <gaurrit8@gmail.com>
2025-12-17 23:19:37 +0530

    [Garvit Gaur] Add . Finding the sum of the digits

---
+++ core-java-practice/leet-code-codebase/sumOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n= s.nextInt(); // number whose digits will be added
+
+        int sum= 0;
+
+        // taking the digits from last and adding it to sum
+        while(n>0){
+            sum+= n % 10;
+            n= n/10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
