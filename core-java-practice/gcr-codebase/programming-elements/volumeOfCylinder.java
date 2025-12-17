b7263681805e1ce052184beec21f4017cf6c0449
Garvit <gaurrit8@gmail.com>
2025-12-17 21:56:58 +0530

    {Garvit Gaur] Add . Volume of a Cylinder

---
+++ core-java-practice/gcr-codebase/programming-elements/volumeOfCylinder.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class volumeOfCylinder{
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        
+        System.out.println("Enter the radius: ");
+        double r= s.nextDouble(); // Enter the radius
+        System.out.println("Enter the height: ");
+        double h= s.nextDouble(); // Emter the height
+
+        // Volume of the cylinder
+        double volume= 3.14 * r * r * h;
+        System.out.println("Volume of Cylinder: "+ volume);
+    }
+}
