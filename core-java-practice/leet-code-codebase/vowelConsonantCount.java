5bbf10a07047bc1f61bf9766aa7a3826bab85f0c
Garvit <gaurrit8@gmail.com>
2025-12-17 23:20:12 +0530

    [Garvit Gaur] Add . Counting number of vowels and consonant in a string

---
+++ core-java-practice/leet-code-codebase/vowelConsonantCount.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class vowelConsonantCount {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str= s.nextLine().toLowerCase();
+
+        int vow= 0;
+        int cons= 0;
+
+        for(int i=0;i<str.length();i++){
+            char ch= str.charAt(i);
+
+            if(ch>='a' && ch<= 'z'){ // check letters only
+                if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u'){
+                    vow++;
+                }
+                else{
+                    cons++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vow);
+        System.out.println("Consonants: " + cons);
+    }
+}
