1edc11d7b106d081b07c10ac9337be96f046b87d
Garvit <gaurrit8@gmail.com>
2025-12-17 21:56:11 +0530

    {Garvit Gaur] Add . Calculation of power

---
+++ core-java-practice/gcr-codebase/programming-elements/powerCalculation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PowerCalc {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        System.out.print("Enter base: ");
+        double base= s.nextDouble();// base number
+
+        System.out.print("Enter exponent: ");
+        double exponent= s.nextDouble(); // exponent
+
+        // Calculating power
+        double result= Math.pow(base,exponent);
+        System.out.println("Result= "+ result);
+    }
+}
