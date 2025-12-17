122fc4bc463853024faedd69fba1f6cad6c9aa8e
Garvit <gaurrit8@gmail.com>
2025-12-17 21:55:51 +0530

    {Garvit Gaur] Add . Perimeter of a rectangle

---
+++ core-java-practice/gcr-codebase/programming-elements/perimeter.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class perimeter{
+	public static void main (String [] args){
+
+	Scanner s= new Scanner(System.in);
+    
+    System.out.println("Enter the length: ");
+	int l= s.nextInt(); // Length
+    System.out.println("Enter the breath: ");
+    int b= s.nextInt(); // Breadth
+
+    // perimeter of rectangle
+	int perimeter= 2*(l+b);
+	System.out.println("perimeter of rectangle: "+ perimeter);
+	}
+}
