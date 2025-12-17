f2099f5050ff35121270d7a7cd35c82e569be796
Garvit <gaurrit8@gmail.com>
2025-12-17 23:18:47 +0530

    [Garvit Gaur] Add . Calculating table of a number

---
+++ core-java-practice/leet-code-codebase/multiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class multiplicationTable {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n= s.nextInt();   // Number whose table is calculated
+
+        // this loop is to caculate the table
+        for(int i=1;i<=10;i++){
+            System.out.println(n+ " x "+i+" = "+(n * i));
+        }
+    }
+}
