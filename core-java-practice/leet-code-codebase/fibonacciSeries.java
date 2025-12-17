befdf3297598feb84da7a8662c96665ba1de7def
Garvit <gaurrit8@gmail.com>
2025-12-17 23:17:48 +0530

    [Garvit Gaur] Add . Fibonacci Series

---
+++ core-java-practice/leet-code-codebase/fibonacciSeries.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n= s.nextInt();
+
+        // First two terms
+        int a= 0;
+        int b= 1;   
+
+        System.out.print("Fibonacci Series: ");
+
+        for(int i= 1;i<=n;i++){
+            System.out.print(a+ " ");
+
+            int next = a + b;  // Next term
+            a = b;             // Shift forward
+            b = next;
+        }
+    }
+}
