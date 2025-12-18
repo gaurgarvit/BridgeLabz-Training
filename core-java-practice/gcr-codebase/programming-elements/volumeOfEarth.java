2b3b406cbaa7c72f8bfed582ffbc0b65ee411a73
Garvit <gaurrit8@gmail.com>
2025-12-18 16:00:14 +0530

    [Garvit Gaur] Add . Implement volume calculation of the Earth in cubic kilometers and miles

---
+++ core-java-practice/gcr-codebase/programming-elements/volumeOfEarth.java
@@ -0,0 +1,17 @@
+public class volumeOfEarth {
+    public static void main(String[] args) {
+        // Radius of Earth in kilometers
+        double radiusKm= 6378;
+        
+        // Calculate Volume in Cubic Kilometers
+        // Volume of a sphere = (4/3) * π * r^3
+        double volumeKm= (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+        
+        // Converting radius to miles (1 km= 0.621371 miles)
+        double radiusMiles= radiusKm / 1.6;
+        double volumeMiles= (4.0 / 3.0) * Math.PI * Math.pow(radiusMiles, 3);
+
+        System.out.println("Volume of earth in cubic kilometers is " + volumeKm);
+        System.out.println("Volume of earth in cubic miles is " + volumeMiles);
+    }
+}
