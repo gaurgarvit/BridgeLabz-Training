736fd6263bc8559dfbfca0f60bbb06c887e4ee16
Garvit <gaurrit8@gmail.com>
2025-12-18 11:55:28 +0530

    [Garvit Gaur] Add . Implement average calculation for PCM subjects

---
+++ core-java-practice/gcr-codebase/programming-elements/averageOfSubjects.java
@@ -0,0 +1,12 @@
+public class averageOfSubjects {
+    public static void main(String[] args) {
+        // Marks of PCM
+        int maths= 94;
+        int physics= 95;
+        int chemistry= 96;
+        
+        // calculating average of PCM
+        double average= (maths+physics+chemistry)/ 3.0;
+        System.out.println("Sam's average mark in PCM is "+ average);
+    }
+}
