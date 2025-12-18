6404b80ed5ace6ac2217c311e6401922ca100fed
Garvit <gaurrit8@gmail.com>
2025-12-18 15:59:28 +0530

    [Garvit Gaur] Add . Implement purchase price calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/purchasePrice.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class purchasePrice {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        // taking unit price as input
+        System.out.print("Enter the unit price (INR): ");
+        double unitPrice= s.nextDouble();
+
+        // taking quantity as input
+        System.out.print("Enter the quantity: ");
+        int quantity= s.nextInt();
+
+        // calculation of total purchase price
+        double totalPrice= unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + totalPrice + 
+                           " if the quantity " + quantity + 
+                           " and unit price is INR " + unitPrice);
+    }
+}
