b0b9b15c215b4987dd67a64ace39975caf49be19
Garvit <gaurrit8@gmail.com>
2025-12-18 16:30:49 +0530

    {Garvit Gaur] Add . Implement quotient and remainder calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/quotientRemainder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class quotientRemainder {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        // taking number 1 as input
+        System.out.print("Enter number 1: ");
+        int n1= s.nextInt();
+
+        // taking number 2 as input
+        System.out.print("Enter number 2: ");
+        int n2= s.nextInt();
+
+        // calculation of quotient and remainder
+        int quotient= n1/n2;
+        int remainder= n1%n2;
+
+        System.out.println("The Quotient is " + quotient 
+                    + " and Reminder is " + remainder 
+                    + " of two number " + n1 + " and " + n2);
+    }
+}
