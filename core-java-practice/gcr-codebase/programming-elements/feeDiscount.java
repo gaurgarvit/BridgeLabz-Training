e651ca898a9dca652a9ebf735758ed66f1498095
Garvit <gaurrit8@gmail.com>
2025-12-18 15:58:05 +0530

    [Garvit Gaur] Add . Implement fee discount calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/feeDiscount.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class feeDiscount {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        
+        // taking student fee
+        System.out.print("Enter the Student Fee: ");
+        double fee = s.nextDouble();
+        
+        // taking discount percentage
+        System.out.print("Enter the University Discount Percentage: ");
+        double discountPerc= s.nextDouble();
+        
+        // calculating discount amount
+        double discountAmount= (discountPerc / 100)* fee;
+        
+        // calculation of final fees after discount
+        double finalFee= fee - discountAmount;
+        System.out.println("The discount amount is INR " + discountAmount + 
+                           " and final discounted fee is INR " + finalFee);
+    }
+}
