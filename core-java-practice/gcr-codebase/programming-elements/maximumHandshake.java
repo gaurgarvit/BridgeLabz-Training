77c578840595b66a9d76d68aac5a2643f478df6e
Garvit <gaurrit8@gmail.com>
2025-12-18 15:59:16 +0530

    [Garvit Gaur] Add . Implement maximum handshake calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/maximumHandshake.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class maximumHandshake {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        // taking number of students as input
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = s.nextInt();
+
+        // formula for maximum handshakes
+        int handshakes= (numberOfStudents * (numberOfStudents - 1)) / 2;
+        System.out.println("Maximum possible handshakes: " + handshakes);
+    }
+}
