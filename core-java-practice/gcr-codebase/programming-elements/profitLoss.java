9d21fed5659406c50fd5713113ce218ec710322d
Garvit <gaurrit8@gmail.com>
2025-12-18 11:59:22 +0530

    [Garvit Gaur] Add . Implement profit and profit percentage calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/profitLoss.java
@@ -0,0 +1,17 @@
+public class profitLoss {
+    public static void main(String[] args) {
+        // CP and SP
+        double cp= 129;
+        double sp= 191;
+        
+        // calculation of profit using CP and SP
+        double profit= sp - cp;
+        
+        // calculation of profit percentage
+        double profitPercentage = (profit / cp) * 100;
+        System.out.println("Cost Price: Rs " + cp + 
+                           "\nSelling Price: Rs " + sp + 
+                           "\nProfit: Rs " + profit + 
+                           "\nProfit Percentage: " + profitPercentage + " %");
+    }
+}
