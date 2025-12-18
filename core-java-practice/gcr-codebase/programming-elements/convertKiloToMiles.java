ff0dd79e64b7fc68c7c7e8e4779e0a7033535250
Garvit <gaurrit8@gmail.com>
2025-12-18 11:55:46 +0530

    [Garvit Gaur] Add . Implement kilometer to miles conversion program

---
+++ core-java-practice/gcr-codebase/programming-elements/convertKiloToMiles.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class convertKiloToMiles {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        System.out.print("Enter kilometers: ");
+        double kilometers = s.nextDouble(); // Distance in kilometers
+
+        // Convert km to miles
+        double miles = kilometers * 0.621371;
+        System.out.println("Miles: "+ miles);
+    }
+}
