3fe888ca586f4b603fc8e804bb34895efaa006cf
Garvit <gaurrit8@gmail.com>
2026-01-30 11:55:28 +0530

    [Garvit Gaur] Add . logging transaction IDs with current timestamps

---
+++ java8-feature-practice/gcr-codebase/stream_api/TransactionLogger.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class TransactionLogger {
+    public static void main(String[] args) {
+        List<String> transactionIds = Arrays.asList("TXN001", "TXN002", "TXN003");
+
+        transactionIds.forEach(id -> 
+            System.out.println(LocalDateTime.now() + " Transaction: " + id)); // printing id with time
+    }
+}
